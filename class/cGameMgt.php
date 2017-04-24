cb1a06fb6fe947b640983193cc01904dc39b5382
ben <ben.soille@gmail.com>
2017-04-24 15:26:43 +0200

    Ajour methode draw echiquier

--- class/cGameMgt.php
+++ class/cGameMgt.php
@@ -3,14 +3,16 @@
 class cGameMgt {
   private $id ;
   private $score ;
+  private $boardSize ;
   private $playersList    = [] ;
   private $piecesList     = [] ;
   private $toursHistory   = [] ;
   private $dates          = array('begin'=>null, 'expire'=>null) ;
 
-  function __construct() {
+  function __construct($boardSize = 8) {
     echo 'Constructing...' ;
-    $this->dates['begin'] = new DateTime() ;
+    $this->dates['begin']   = new DateTime() ;
+    $this->boardSize        = $boardSize ;
   }
 
   function __toString() {
@@ -20,8 +22,41 @@ class cGameMgt {
       'playersList'   => $this->playersList,
       'piecesList'    => $this->piecesList,
       'toursHistory'  => $this->toursHistory,
-      'dates'          => $this->dates
+      'dates'         => $this->dates,
+      'boardSize'     => $this->boardSize
     ) ;
+  }
+
+  function drawChessBoard() {
+
+    for($rows=0; $rows<$this->boardSize; $rows++) {
+      // For each line :
+
+      // Write upper border
+      for($cols=0; $cols<$this->boardSize; $cols++) {
+        echo '---';
+      }
+echo PHP_EOL ;
+      // Write first vertical separator
+      for($cols=0; $cols<$this->boardSize; $cols++) {
+        echo '|  ';
+        if($cols+1 == $this->boardSize) echo '|' ;
+      }
+echo PHP_EOL ;
+      // Write 2nd vertical separator
+      for($cols=0; $cols<$this->boardSize; $cols++) {
+        echo '|  ';
+        if($cols+1 == $this->boardSize) echo '|' ;
+      }
+
+      echo PHP_EOL ;
+    }
+    // Write bottom border
+    for($cols=0; $cols<$this->boardSize; $cols++) {
+      echo '---';
+    }
+    echo PHP_EOL ;
+
   }
 
 }
